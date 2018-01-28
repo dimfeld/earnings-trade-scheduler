082c5bb20ee496901a7203d495b7192c7d755534
Daniel Imfeld <daniel@danielimfeld.com>
2018-01-27 21:38:06 -1000

    Everything is working. Add some comments

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,7 @@ fn init_logger() -> slog::Logger {
         .expect("building logger")
 }
 
+#[derive(Debug)]
 struct TestsAndEarnings {
     symbol : String,
     best_test: cmlviz::BacktestResult,
@@ -47,24 +48,24 @@ struct TestsAndEarnings {
 }
 
 fn main() {
-    let mut logger = init_logger();
-
+    let logger = init_logger();
     let filename = std::env::args().nth(1).expect("filename");
-    // let symbol = std::env::args().nth(1).expect("symbol");
-
 
     let mut reader = csv::ReaderBuilder::new()
         .has_headers(true)
-        .from_path(filename)
+        .from_path(&filename)
         .expect("opening csv");
 
 
+    // Read the file and group the tests by symbol.
+    info!(logger, "Reading file {}", filename);
     let backtests_by_symbol = reader.deserialize::<cmlviz::BacktestResultInput>()
         .into_iter()
         .map(|t| cmlviz::BacktestResult::from_input(t?))
         .fold(HashMap::<String, Vec<cmlviz::BacktestResult>>::new(), |mut acc, test| {
+            // Assume that the CSV data is proper so we don't do real error handling on it.
             let test = test.expect("csv row");
-            println!("{:?}", test);
+
             acc
                 .entry(test.symbol.clone())
                 .or_insert_with(Vec::new)
@@ -72,9 +73,6 @@ fn main() {
             acc
         });
 
-
-    // For each symbol, get the earnings date info
-    // Given the best guess at earnings, spit out the possible open/close dates for each scenario, sorted by the average trade return.
 
     let mut headers = Headers::new();
     headers.set(UserAgent::new("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"));
@@ -87,6 +85,9 @@ fn main() {
     let tests_with_earnings = backtests_by_symbol
         .into_iter()
         .map(|(symbol, tests)| {
+            info!(logger, "Processing symbol {}", symbol);
+
+            // Figure out our best guess at the earnings date based on the CML data and a bunch of other sources.
             let mut earnings_dates = earnings::get_earnings_date_estimates(&logger, &client, symbol.as_str());
             let test_date = earnings::SourcedEarningsTime{
                 source: "CML",
@@ -95,6 +96,10 @@ fn main() {
             earnings_dates.push(test_date);
 
             let guess = earnings::best_earnings_guess(&earnings_dates);
+
+            // The "best test" is just the one that has the highest average trade return.
+            // In general the win rates for the various strategies are close enough that it's not worth factoring it in
+            // beyond the effect that it already has on the average return.
             let best_test = cmlviz::get_best_test(&tests);
 
             TestsAndEarnings{
@@ -106,15 +111,8 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    // for (symbol, backtests) in backtests_by_symbol {
-
-    //     for x in &earnings_dates {
-    //         println!("{} - {}", x.source, x.datetime);
-    //     }
-
-
-    //     println!("Best guess: {:?}", guess);
-    // }
-
-
+    // TODO Nice output formatting
+    for x in tests_with_earnings {
+        println!("{:?}", x);
+    }
 }
